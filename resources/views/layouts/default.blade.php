84d06b9cc1b7e21f63a84a9d4712c142de676f39
Mybabyfirst <378185979@qq.com>
2020-03-15 12:05:42 +0800

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
@@ -15,5 +16,6 @@
         @include('layouts._footer')
     </div>
 </div>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
